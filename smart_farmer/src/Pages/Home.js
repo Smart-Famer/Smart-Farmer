bef65b6cdb400423e1a41540707489ca7f734bb6
MadaraSemini <pgmsgunarathna@gmail.com>
2022-09-27 03:24:12 +0530

    created login form

--- smart_farmer/src/Pages/Home.js
+++ smart_farmer/src/Pages/Home.js
@@ -1,8 +1,12 @@
+import Login from "../components/Login";
+
 export default function Home()
 {
     return(
         <div className="home">
-            <h1>Home</h1>
+            <div className="home-header"><h1>Home</h1></div>
+            <div className="home-login"><Login /></div>
+            
         </div>
     )
 }
