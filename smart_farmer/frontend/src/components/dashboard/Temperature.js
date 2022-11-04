07f2e034311a9eb228df077bc617639770071e89
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-11-05 04:06:31 +0530

    temperatures

--- smart_farmer/frontend/src/components/dashboard/Temperature.js
+++ smart_farmer/frontend/src/components/dashboard/Temperature.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Row,Col } from "reactstrap";
 
 export default function Temperature(){
+
+    const sourceIds = ['Temp_5','Temp_6']
+    const [temperatures, setTemperatures] = useState([])
+
+    useEffect(() => {
+      const fetchTemperature = async () => {
+        const temps =  sourceIds.map(async (sourceId)=>{
+            const response = await fetch(`http://localhost:4000/api/datareading/${sourceId}`)
+            const json = await response.json()
+
+            console.log(json)
+            if (response.ok) {
+                return json.reading
+            }
+        })
+         setTemperatures(temps)
+      }
+  
+      fetchTemperature()
+    }, [])
+
+    const components = temperatures.map((t)=>{
+        <Col ><h3 className="bg-secondary bg-opacity-25 rounded py-3 text-center">{t}&deg;C</h3></Col>
+    })
+
     return(
         <Row>
-            <Col ><h3 className="bg-secondary bg-opacity-25 rounded py-3 text-center">27&deg;C</h3></Col>
-            <Col ><h3 className="bg-secondary bg-opacity-25 rounded py-3 text-center">27&deg;C</h3></Col>
+            {components}
         </Row>
     )
 }
