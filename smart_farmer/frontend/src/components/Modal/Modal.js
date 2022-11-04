a09791469f71a0df6f41fef5af1b62635abfb204
mad <pgmsgunarathna@gmail.com>
2022-11-04 17:33:53 +0530

    modal created

---
+++ smart_farmer/frontend/src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Button, Modal, ModalFooter,
+    ModalHeader, ModalBody
+} from "reactstrap"
+
+export default function Modal(Props) {
+    // Modal open state
+    const [modal, setModal] = React.useState(false);
+  
+    // Toggle for Modal
+    const toggle = () => setModal(!modal);
+  
+    return (
+        <div style={{
+            display: 'block', width: 700, padding: 30
+        }}>
+            {/* <h4>ReactJS Reactstrap Modal Component</h4>
+            <Button color="danger"
+                onClick={toggle}>Click me to open Modal</Button> */}
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader
+                    toggle={toggle}>{Props.title}</ModalHeader>
+                <ModalBody>
+                    {Props.message}
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="green" onClick={toggle}>Okay</Button>
+                </ModalFooter>
+            </Modal>
+        </div >
+    );
+    
+
+}
