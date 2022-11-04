3cf21beba4c7b063c0a8c1659f860a523e8e2ebf
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-11-05 03:36:02 +0530

    rainfall

--- smart_farmer/frontend/src/components/dashboard/RainFall.js
+++ smart_farmer/frontend/src/components/dashboard/RainFall.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Row,Col} from "reactstrap";
 
 export default function RainFall(props){
+    const sourceId = 'Rain_7'
+    const [rainfall, setRainfall] = useState(null)
+
+    useEffect(() => {
+      const fetchRainFall = async () => {
+        const response = await fetch(`http://localhost:4000/api/datareading/${sourceId}`)
+        const json = await response.json()
+
+        if (response.ok) {
+            setRainfall(json)
+        }
+      }
+  
+      fetchRainFall()
+    }, [])
     return(
         <Row className="px-5">
-            <Col ><h1 className="bg-secondary bg-opacity-25 rounded py-3 text-center">5mm</h1></Col>
+            <Col ><h1 className="bg-secondary bg-opacity-25 rounded py-3 text-center">{rainfall && (rainfall.reading)} mm</h1></Col>
         </Row>
     )
 }
--- smart_farmer/simulator/server.js
+++ smart_farmer/simulator/server.js
@@ -1,18 +1,43 @@
 const axios = require('axios');
 
 const sendReading = (type)=>{
-    temperature = Math.random()*50
-    axios.post(`http://localhost:4000/api/fetch-data/${type}`,{
-        temperature
+    reading = (Math.random()*50).toFixed(2)
+    let timestamp = new Date().toJSON();
+    source_index = Math.round(Math.random()*10)
+    temp_array = ['Temp_1','Temp_2','Temp_3','Temp_4','Temp_5','Temp_6','Temp_7','Temp_8','Temp_9','Temp_10']
+    hum_array = ['Hum_1','Hum_2','Hum_3','Hum_4','Hum_5','Hum_6','hum_7','hum_8','hum_9','hum_10']
+    rain_array = ['Rain_1','Rain_2','Rain_3','Rain_4','Rain_5','Rain_6','Rain_7','Rain_8','Rain_9','Rain_10']
+    source_id = ''
+    switch (type) {
+      case 'temp':
+        sourceId=temp_array[source_index]
+        break;
+      case 'hum':
+        sourceId=hum_array[source_index]
+        break;
+      case 'rain':
+        sourceId=rain_array[source_index]
+        break;
+    
+      default:
+        break;
+    }
+    if(typeof reading !== 'undefined' && typeof sourceId !== 'undefined'){
+      axios.post(`http://localhost:4000/api/datareading/`,{
+        sourceId,
+        reading,
+        timestamp
     })
   .then((response) => {
-    console.log(response.data.msg);
+    console.log(response.data.reading);
   })
   .catch((error) => {
     console.log("error");
   });
+    }
+    
 }
-
-setInterval(sendReading,10000,'temp')
-setInterval(sendReading,7000,'humidity')
-setInterval(sendReading,10000,'rainfall')
+// sendReading('temp')
+setInterval(sendReading,5000,'temp')
+setInterval(sendReading,6000,'hum')
+setInterval(sendReading,7000,'rain')
