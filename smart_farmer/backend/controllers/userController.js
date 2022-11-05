a3d52ac61ef35f37e35f86dc0b97015ed9744aae
Hiruna Harankahadeniya <hiruna.19@cse.mrt.ac.lk>
2022-11-05 08:27:13 +0530

    teperature done

--- smart_farmer/backend/controllers/userController.js
+++ smart_farmer/backend/controllers/userController.js
@@ -2,14 +2,16 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken=({_id, email, user_type, first_name, second_name, location, profile_picture})=>{
-    return jwt.sign({_id, email, user_type, first_name, second_name, location, profile_picture, farms},process.env.SECRET, {expiresIn:"1d"})
+    return jwt.sign({_id, email, user_type, first_name, second_name, location, profile_picture},process.env.SECRET, {expiresIn:"1d"})
 }
 const login = async (req,res)=>{
     data=req.body
     try{
         const user = await User.signIn(data)
+        // console.log(user)
         const token = createToken(user)
-        const {_id, email, user_type, first_name, second_name, location, profile_picture, farms} = user
+        const {_id, email, user_type, first_name, second_name, location, profile_picture,farms} = user
+        // console.log(farms)
         res.status(200).json({details:{
             _id, email, user_type, first_name, second_name, location, profile_picture, farms
         }, token})
