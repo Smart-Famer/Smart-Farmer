83251a9f754d5d43426e11287373bf2d6071a406
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-11-09 09:39:54 +0530

    getcropyields_backend

--- smart_farmer/backend/controllers/cropYieldController.js
+++ smart_farmer/backend/controllers/cropYieldController.js
@@ -1,6 +1,27 @@
 const cropYieldModel = require('../models/cropYieldModel')
 const mongoose = require('mongoose')
 
+const getYield = async(req,res)=>{
+    const {farm_id} = req.params
+    const date = new Date()
+    const year = date.getFullYear()
+    const start = `${year}-01-01`
+    const end = `${year}-12-31`
+
+    try {
+        const yieldData = await cropYieldModel.find(
+            {farm_id:farm_id,date:{$gte:start,$lte:end}}
+        )
+        if(!yieldData){
+            res.status(404).json({error:`No yield data for this farm in the date range of ${start} to ${end}`})
+        }
+        res.status(200).json(yieldData)
+    } catch (error) {
+        console.log(error)
+        res.status(404).json(error)
+    }
+}
+
 const createYield = async(req,res)=>{
     const {farm_id,crop_name,date,yield} = req.body
     try{
@@ -16,4 +37,4 @@ const createYield = async(req,res)=>{
         res.json(error)
     }
 }
-module.exports = {createYield}
+module.exports = {createYield,getYield}
