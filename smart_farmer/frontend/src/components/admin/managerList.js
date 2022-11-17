922804bcdd7a311cf91173bd3bdd7294c318fd70
mad <pgmsgunarathna@gmail.com>
2022-11-17 21:12:51 +0530

    manager farm list added

--- smart_farmer/frontend/src/components/admin/managerList.js
+++ smart_farmer/frontend/src/components/admin/managerList.js
@@ -3,9 +3,36 @@ import { Table } from "reactstrap";
 import { useState } from "react";
 
 
+function AssistFarm(props) {
+  const [farm_list, setList] = useState();
+  // const [components_list, setComponents] = useState()
+  // console.log(farm_list)
+  const fetchFarms = async (id) => {
+    // console.log(id);
+    const response = await fetch(
+      `${process.env.REACT_APP_HOST}/api/farm/get-user-farms/${id}`
+    );
+    const json = await response.json();
+    // alert(JSON.stringify(json.length))
+    // console.log(json)
+    if (response.ok) {
+      // console.log(json);
+      // setFarmList(json);
+      let tempList = json.map((farm) => <li>{farm.name}</li>);
+      if (tempList.length != 0) setList(tempList);
+      else setList(<li>{"No Farms"}</li>);
+    }
+  };
+  useEffect(() => {
+    fetchFarms(props?._id);
+  }, []);
+
+  return <ul style={{ listStyleType: "none" }}>{farm_list}</ul>;
+}
+
 export default function ManagerList(content) {
-  const [farm_list, setFarmList] = useState([]);
-  const [farm_iteam_list, setFarmIteamList] = useState([]);
+  // const [farm_list, setFarmList] = useState([]);
+  // const [farm_iteam_list, setFarmIteamList] = useState([]);
 
   // const fetchFarms = async (farm) => {
   //   console.log(farm);
@@ -15,40 +42,40 @@ export default function ManagerList(content) {
   //   const json = await response.json();
   //   console.log(json);
   //   if (response.ok) {
-      // setFarmList([...farm_list, json]);
+  //     setFarmList([...farm_list, json]);
   //   }
   // };
-  useEffect(() => {
-    const fetchFarms = async (id_list) => {
-      const response = await fetch(
-        `${process.env.REACT_APP_HOST}/api/manager/get-farms`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ farm_ids: id_list }),
-        }
-      );
-      const json = await response.json();
-      // alert(JSON.stringify(json.length))
+  // useEffect(() => {
+  //   const fetchFarms = async (id_list) => {
+  //     const response = await fetch(
+  //       `${process.env.REACT_APP_HOST}/api/manager/get-farms`,
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ farm_ids: id_list }),
+  //       }
+  //     );
+  //     const json = await response.json();
+  //     alert(JSON.stringify(json.length))
 
-      //console.log(json)
-      if (response.ok) {
-        console.log(json);
-        setFarmList([...farm_list,json]);
-      }
-    };
+  //     console.log(json)
+  //     if (response.ok) {
+  //       console.log(json);
+  //       setFarmList([...farm_list,json]);
+  //     }
+  //   };
 
-    content.iteamList.forEach((element) => {
-      console.log(element.farms);
-      fetchFarms(element.farms);
-    });
-  }, []);
+  //   content.iteamList.forEach((element) => {
+  //     console.log(element.farms);
+  //     fetchFarms(element.farms);
+  //   });
+  // }, []);
 
-  console.log(farm_list);
+  // console.log(farm_list);
 
-  // const listComponent =
+
   return (
     <Table striped>
       <thead>
@@ -67,6 +94,7 @@ export default function ManagerList(content) {
             <td>{manager.second_name}</td>
             <td>{manager.email}</td>
             <td>
+            <AssistFarm _id={manager._id} />
             </td>
             <td>{manager.location}</td>
           </tr>
