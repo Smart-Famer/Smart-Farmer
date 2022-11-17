3360fdd7d82a1500b93d9efc71ade6a845a2ffdf
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-11-17 23:46:19 +0530

    Update HistoricalNpkPage.js

--- smart_farmer/frontend/src/Pages/HistoricalNpkPage.js
+++ smart_farmer/frontend/src/Pages/HistoricalNpkPage.js
@@ -14,8 +14,34 @@ export default function HistoricalNPKpage() {
     <div className="main-container">
       <Sidebar />
       <div>
-        {/* <LineChart
-        /> */}
+        <LineChart
+          xAxisLabel="Date"
+          yAxisLabel="Temperature"
+          xAxisValues={[
+            "23/09/2022",
+            "24/09/2022",
+            "25/09/2022",
+            "26/09/2022",
+            "27/09/2022",
+            "28/09/2022",
+          ]}
+
+          chartTitle="Historical Data"
+          dataSets={[
+            {
+              label: "Sensor 1",
+              data: [27, 26, 25.5, 28, 27, 30],
+              borderColor: "rgb(255, 99, 132)",
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+            {
+              label: "Sensor 2",
+              data: [25, 25.5, 26.5, 28, 26.5, 26],
+              borderColor: "rgb(255, 200, 100)",
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+          ]} 
+        />
       </div>
     </div>
   );
