db44360e6c5ca26e895044c80922bb8691b0cb81
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-11-18 12:42:35 +0530

    bug fix

--- smart_farmer/frontend/src/components/Sidebar/SideBar1.js
+++ smart_farmer/frontend/src/components/Sidebar/SideBar1.js
@@ -4,28 +4,50 @@ import { Link, Navigate } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import "./Sidebar.css";
 
-
-export default function Sidebar1(){
-    const {farm} = useFarmContext()
-    const {user} = useAuthContext()
-    return(
-//         <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-//   Link with href
-// </a>
-// <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-//   Button with data-bs-target
-// </button>
-<>
-<div className="offcanvas offcanvas-start w-25" tabindex="-1" id="offcanvas" data-bs-keyboard="false" data-bs-backdrop="true" data-bs-scroll="true" style={{maxWidth:200}}>
-    <div className="offcanvas-header  text-white">
-        <h5 className="offcanvas-title d-none d-sm-block fw-bold" id="offcanvas">{farm?.name}</h5>
-        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div className="offcanvas-header text-white">
-        <h6 className="offcanvas-title d-none d-sm-block" id="offcanvas">{farm?.address}</h6>
-    </div>
-    <div className="offcanvas-body px-0">
-        <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
+export default function Sidebar1() {
+  const { farm } = useFarmContext();
+  const { user } = useAuthContext();
+  return (
+    //         <a className="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+    //   Link with href
+    // </a>
+    // <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+    //   Button with data-bs-target
+    // </button>
+    <>
+      <div
+        className="offcanvas offcanvas-start w-25"
+        tabIndex="-1"
+        id="offcanvas"
+        data-bs-keyboard="false"
+        data-bs-backdrop="true"
+        data-bs-scroll="true"
+        style={{ maxWidth: 200 }}
+      >
+        <div className="offcanvas-header  text-white">
+          <h5
+            className="offcanvas-title d-none d-sm-block fw-bold"
+            id="offcanvas"
+          >
+            {farm?.name}
+          </h5>
+          <button
+            type="button"
+            className="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-header text-white">
+          <h6 className="offcanvas-title d-none d-sm-block" id="offcanvas">
+            {farm?.address}
+          </h6>
+        </div>
+        <div className="offcanvas-body px-0">
+          <ul
+            className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start"
+            id="menu"
+          >
             <li className="nav-item fs-5 ">
               <Link
                 className="nav-link text-truncate"
--- smart_farmer/frontend/src/components/dashboard/RecentPhotos.js
+++ smart_farmer/frontend/src/components/dashboard/RecentPhotos.js
@@ -1,72 +1,120 @@
 import React from "react";
-import {Row,Col } from "reactstrap";
-import farmImage1 from './images/farmImage1.jpg'
-import farmImage2 from './images/farmImage2.jpg'
-import "../../App.css"
+import { Row, Col } from "reactstrap";
+import farmImage1 from "./images/farmImage1.jpg";
+import farmImage2 from "./images/farmImage2.jpg";
+import "../../App.css";
 
-export default function RecentPhotos(){
-    return(
-        <div className="container ">
-            <div className="row mb-3">
-                <div className="col-12 col-md-6 mb-4">
-                    <div className="row">
-                        <div className="col">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Date</label>
-                        </div>
-                        <div className="col">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="02/09/2022"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Time</label>
-                        </div>
-                        <div className="col">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="16:33"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Camera Angle</label>
-                        </div>
-                        <div className="col">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="1"/>
-                        </div>
-                    </div>
-                    <div className="row mt-1">
-                        <img className="w-75" src={farmImage1} alt="farmImage1"/>
-                    </div>
-                </div>
-                <div className="col-12 col-md-6">
-                    <div className="row">
-                        <div className="col">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Date</label>
-                        </div>
-                        <div className="col">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="02/09/2022"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Time</label>
-                        </div>
-                        <div className="col">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="16:33"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Camera Angle</label>
-                        </div>
-                        <div className="col">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="1"/>
-                        </div>
-                    </div>
-                    <div className="row mt-1">
-                        <img className="w-75" src={farmImage1} alt="farmImage1"/>
-                    </div>
-                </div>
+export default function RecentPhotos() {
+  return (
+    <div className="container ">
+      <div className="row mb-3">
+        <div className="col-12 col-md-6 mb-4">
+          <div className="row">
+            <div className="col">
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Date
+              </label>
             </div>
+            <div className="col">
+              <input
+                type="text"
+                readOnly
+                className="form-control-plaintext"
+                id="staticEmail"
+                value="02/09/2022"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Time
+              </label>
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                readOnly
+                className="form-control-plaintext"
+                id="staticEmail"
+                value="16:33"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Camera Angle
+              </label>
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                readOnly
+                className="form-control-plaintext"
+                id="staticEmail"
+                value="1"
+              />
+            </div>
+          </div>
+          <div className="row mt-1">
+            <img className="w-75" src={farmImage1} alt="farmImage1" />
+          </div>
         </div>
+        <div className="col-12 col-md-6">
+          <div className="row">
+            <div className="col">
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Date
+              </label>
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                readOnly
+                className="form-control-plaintext"
+                id="staticEmail"
+                value="02/09/2022"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Time
+              </label>
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                readOnly
+                className="form-control-plaintext"
+                id="staticEmail"
+                value="16:33"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Camera Angle
+              </label>
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                readOnly
+                className="form-control-plaintext"
+                id="staticEmail"
+                value="1"
+              />
+            </div>
+          </div>
+          <div className="row mt-1">
+            <img className="w-75" src={farmImage1} alt="farmImage1" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
